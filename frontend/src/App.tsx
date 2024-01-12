dd06bf95fd45d263ad393c8579f9ebdd5a3ffce0
dacarva <davidfelipec88@gmail.com>
2024-01-12 18:27:29 -0500

    Set some styles to test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,12 @@ import { ConnectKitButton } from "connectkit";
 
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <ConnectKitButton />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
+
+      <div className="flex items-center justify-center h-screen">
+        <ConnectKitButton />
+      </div>
     </div>
   );
 }
